7b39eb750c516e2ce669ddacd84774854a69a478
Jihun-Hwang <goharrm@gmail.com>
2023-01-22 14:30:49 +0900

    fix: RandomNicknameGenerator 생성자가 'classpath:nickname/'를 읽지 못하는 문제 임시 대비

--- src/main/java/com/prgrms/offer/authentication/application/RandomNicknameGenerator.java
+++ src/main/java/com/prgrms/offer/authentication/application/RandomNicknameGenerator.java
@@ -3,6 +3,7 @@ package com.prgrms.offer.authentication.application;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
@@ -15,20 +16,25 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class RandomNicknameGenerator {
 
-    private final List<String> adjectives;
-    private final List<String> nouns;
+    private List<String> adjectives;
+    private List<String> nouns;
 
-    public RandomNicknameGenerator(@Value("classpath:nickname/nickname_adjectives.txt") Resource adjectivesResource,
-                                   @Value("classpath:nickname/nickname_nouns.txt") Resource nounsResource) {
-        try {
-            this.adjectives = Collections.unmodifiableList(Files.readAllLines(Path.of(adjectivesResource.getURI())));
-            this.nouns = Collections.unmodifiableList(Files.readAllLines(Path.of(nounsResource.getURI())));
-        } catch (IOException e) {
-            log.error("랜덤 닉네임 데이터 파일을 읽는데 실패했습니다. adjectivesResource = {}, nounsResource = {}",
-                    adjectivesResource, nounsResource);
-            throw new RuntimeException(e);
-        }
+    public RandomNicknameGenerator() {   // TODO: 아래 생성자에서 resource/nickname를 못 읽는 문제로 인해 임시 구현
+        this.adjectives = Collections.unmodifiableList(Arrays.asList("싱거운", "빼어난", "서투른"));
+        this.nouns = Collections.unmodifiableList(Arrays.asList("디지털가전", "다리미", "식기세척기"));
     }
+
+//    public RandomNicknameGenerator(@Value("classpath:nickname/nickname_adjectives.txt") Resource adjectivesResource,
+//                                   @Value("classpath:nickname/nickname_nouns.txt") Resource nounsResource) {
+//        try {
+//            this.adjectives = Collections.unmodifiableList(Files.readAllLines(Path.of(adjectivesResource.getURI())));
+//            this.nouns = Collections.unmodifiableList(Files.readAllLines(Path.of(nounsResource.getURI())));
+//        } catch (IOException e) {
+//            log.error("랜덤 닉네임 데이터 파일을 읽는데 실패했습니다. adjectivesResource = {}, nounsResource = {}",
+//                    adjectivesResource, nounsResource);
+//            throw new RuntimeException(e);
+//        }
+//    }
 
     public String generateRandomNickname() {
         int adjIndex = ThreadLocalRandom.current().nextInt(0, adjectives.size());
